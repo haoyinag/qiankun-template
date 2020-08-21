1f519523d62645402dfa55ac6e9698d1da62cfdf
陈浩银 <chen.haoyin@angsivip.com>
2020-08-21 09:31:19 +0800

    feat：umi使用antd

--- umi-app/src/pages/index.tsx
+++ umi-app/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import { Button } from 'antd';
+
 import styles from './index.less';
 
 export default () => {
   return (
-    <div>
-      <h1 className={styles.title}>我是子应用【umi-app】</h1>
+    <div className={styles.title}>
+      <Button type="primary">我是子应用【umi-app】</Button>
     </div>
   );
-}
+};
